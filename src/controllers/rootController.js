20582b1f8b389c45f8b0a1b843794a3f266c655a
Regular Kim <82751941+kimregular@users.noreply.github.com>
2024-11-08 14:32:00 +0900

    로그인 프론트 작업

--- src/controllers/rootController.js
+++ src/controllers/rootController.js
@@ -3,13 +3,34 @@ export const home = (req, res) => {
 };
 
 export const getLogin = async (req, res) => {
-    const response = await fetch(`${process.env.BACKEND_URL}/api/v1`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
+    const response = await fetch(
+        `${process.env.BACKEND_URL}${process.env.API_V1}`,
+        {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
         },
-    });
+    );
     const data = await response.json();
     console.log(data);
     return res.status(200).render("login", { pageTitle: "Login" });
+};
+
+export const postLogin = async (req, res) => {
+    const { body } = req;
+    const response = await fetch(
+        `${process.env.BACKEND_URL}${process.env.API_V1}/login`,
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(body),
+        },
+    );
+    console.log(response);
+    // const result = await response.json();
+    // console.log(result);
+    return res.redirect("/");
 };
--- src/routes/rootRouter.js
+++ src/routes/rootRouter.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { getLogin, home } from "../controllers/rootController.js";
+import { getLogin, home, postLogin } from "../controllers/rootController.js";
 
 const rootRouter = express.Router();
 
 rootRouter.route("/").get(home);
-rootRouter.route("/login").get(getLogin);
+rootRouter.route("/login").get(getLogin).post(postLogin);
 
 export default rootRouter;
