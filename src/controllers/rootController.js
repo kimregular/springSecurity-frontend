e3fbf2a7d08eb1d11aa056da012e27669f4b6b2f
Regular Kim <82751941+kimregular@users.noreply.github.com>
2024-11-09 14:56:20 +0900

    코드 정리

--- src/controllers/rootController.js
+++ src/controllers/rootController.js
@@ -9,29 +9,23 @@ export const getJoin = (req, res) => {
 export const postJoin = async (req, res) => {
     const { body: joinRequest } = req;
     console.log(joinRequest);
-    const response = await fetch(
-        `${process.env.BACKEND_URL}${process.env.API_V1}/join`,
-        {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(joinRequest),
+    await fetch(`${process.env.BACKEND_URL}${process.env.API_V1}/join`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
         },
-    );
+        body: JSON.stringify(joinRequest),
+    });
     return res.redirect("/");
 };
 
 export const getLogin = async (req, res) => {
-    const response = await fetch(
-        `${process.env.BACKEND_URL}${process.env.API_V1}`,
-        {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            },
+    await fetch(`${process.env.BACKEND_URL}${process.env.API_V1}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
         },
-    );
+    });
     return res.status(200).render("login", { pageTitle: "Login" });
 };
 
